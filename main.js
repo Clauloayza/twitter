97d441cd0a9abccf53f8c36be067c11e4d6f458e
claudia <clou7loymed@gmail.com>
2017-01-22 21:09:54 -0500

    dando funcionalidad js

---
+++ main.js
@@ -0,0 +1,24 @@
+function limpiarCuadroTexto(){
+    document.getElementById("cuadroTexto").value="";
+}
+
+function checkBox(){
+    
+}
+
+function agregaTarea(){
+    var texto= document.getElementById("cuadroTexto").value;
+    var contenedor= document.createElement("div");
+    var padre= document.getElementById("padremax");
+    padre.appendChild(contenedor);
+    
+    var checkBox= document.createElement.setAttribute("type","checkbox");
+    contenedor.appendChild(checkBox);
+    
+    var elemento= document.createElement("span");
+    var contexto= document.createTextNode(mensaje);
+    elemento.appendChild(contexto);
+    contenedor.appendChild(elemento);
+    
+    limpiarCuadroTexto();
+}
